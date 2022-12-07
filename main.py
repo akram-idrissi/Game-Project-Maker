f5a0bf84314e353665e95b978f7c6fba1d1ab5cc
akram-idrissi <akram.mo.gb@gmail.com>
2022-12-07 11:37:21 +0100

    Added the skeleton of the script

---
+++ main.py
@@ -0,0 +1,20 @@
+import os 
+from subprocess import PIP, run
+
+
+def run_command():
+    pass
+
+
+def get_args():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+    
