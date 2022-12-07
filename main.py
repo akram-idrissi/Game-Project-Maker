010c5892cef87c6bca046d25496cf84c025f577a
akram-idrissi <akram.mo.gb@gmail.com>
2022-12-07 22:27:34 +0100

    Updated the main function

--- main.py
+++ main.py
@@ -22,19 +22,16 @@ def main():
     path = get_args()
     env = dotenv_values('.env')
 
-    if(os.path.exists()):
-        os.chdir(path)
-        mkdir('lib')
-        mkdir('include')
-        
-        run_command(f'COPY {env["SDL_LIB"]} lib')
-        run_command(f'COPY {env["SDL_IMG_LIB"]} lib')
-        run_command(f'COPY {env["SDL_INCLUDE"]} include')
-        run_command(f'COPY {env["SDL_IMG_INCLUDE"]} include')
-        run_command(f'COPY {env["SDL_DLL"]} .')
-        run_command(f'COPY {env["SDL_DLL"]} .')
+    mkdir('lib')
+    mkdir('include')
+
+    run_command(f'COPY {env["SDL_LIB"]} lib')
+    run_command(f'COPY {env["SDL_IMG_LIB"]} lib')
+    run_command(f'COPY {env["SDL_INCLUDE"]} include')
+    run_command(f'COPY {env["SDL_IMG_INCLUDE"]} include')
+    run_command(f'COPY {env["SDL_DLL"]} .')
+    run_command(f'COPY {env["SDL_DLL"]} .')
  
 
 if __name__ == '__main__':
     main()
-
