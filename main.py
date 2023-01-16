30a8f719eae76b3f3a14ccda6f8e791d93e98891
akram-idrissi <akram.mo.gb@gmail.com>
2023-01-16 23:33:13 +0100

    Using shutil instead of subprocess

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 import os 
 import sys
-from subprocess import run
+import shutil
 
 
 SOURCE_PATH='C:\_coding\Templates\games'
-
-
-def run_command(command):
-    run(command, shell=True)
-
 
 def get_args():
     args = sys.argv
     if(len(args) == 2): return args[1]
 
 
-def mkdir(dir):
-    if(not os.path.exists(dir)):
-        os.mkdir(dir)
-
-
 def main():
     destination = get_args()
-    run_command(f'xcopy /QCIE {SOURCE_PATH} {destination}')
+    shutil.copytree(SOURCE_PATH, destination)
  
 
 if __name__ == '__main__':
